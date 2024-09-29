2538b65e6c6ec9f8f6d1ff527b0c5cc676791600
Loganathan P <sathishloga7264@gmail.com>
2024-09-29 15:01:49 +0530

    initial commit

---
+++ Madlibs game.py
@@ -0,0 +1,16 @@
+#Madlibs game
+#word game where you create a story by filling in the blanks with random words
+
+adjective  = input("Enter an adjective (decsription): ")
+animal = input("Enter name of animal: ")
+verb1 = input("Enter a verb: ")
+object= input("Enter a obect name: ")
+verb2 = input("Enter a past tense verb: ")
+verb3 = input("Enter a verb with ending 'ing': ")
+name = print("Enter funny person name: ")
+adjective2 = input("Enter an adjective (description): ")
+
+
+print(f"Today, a {adjective} {animal} tried to {verb1} a {object}.")
+print(f"It {verb2} and started {verb3} like {name}.")
+print(f"It was the most {adjective2} thing ever!")
